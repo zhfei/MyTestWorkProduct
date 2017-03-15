992228b3252ec686b3d456c01e2050ac818e1666
ZHOUFEI <1174977961@qq.com>
2017-03-15 23:57:56 +0800

    add:添加关系运算符

--- MyTestWorkProduct/Other/ShellStudy/test1.sh
+++ MyTestWorkProduct/Other/ShellStudy/test1.sh
@@ -86,6 +86,25 @@ if [ $a != $b ]; then
 fi
 
 # 关系运算符
+if [[ $a -eq $b ]]; then
+	echo "a 等于 b"
+fi
+if [[ $a -ne $b ]]; then
+	echo "a 不等于 b"
+fi
+if [[ $a -gt $b ]]; then
+	echo "a 大于 b"
+fi
+if [[ $a -lt $b ]]; then
+	echo "a 小于 b"
+fi
+if [[ $a -ge $b ]]; then
+	echo "a 大于等于 b"
+fi
+if [[ $a -le $b ]]; then
+	echo "a 小于等于 b"
+fi
+
 
 
 
